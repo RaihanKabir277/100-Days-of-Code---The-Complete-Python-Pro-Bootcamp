04422eec4d8e884ca398dfaf9b86af2cb613d601
RaihanKabir277 <raihank0198@gmail.com>
2025-04-11 11:58:11 +0600

    day 1 added here

---
+++ Day 1/task/task.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+# print("Hello world")
+
+# print("raihan kabir\nhridoy\ndont know")
+
+# print("Hello" + " " + input("What is your name : "))
+
+# print("Hello " + input("What is your name : "))
+
+print("Welcome to the brand name generator")
+city = input("Enter the city name : ")
+pet = input("What is the name of pet : ")
+print("Your brand name should be "+city+ " " +pet)
